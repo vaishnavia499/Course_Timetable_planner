d10085dc6a536ae99065283a0e6e8b08e1b924ae
vaishnavia499 <mvaishnavia1@gmail.com>
2025-08-12 21:24:18 +0530

    Add files via upload

---
+++ AgenticAI.py
@@ -0,0 +1,22 @@
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account (https://dataplatform.cloud.ibm.com/docs/content/wsj/analyze-data/ml-authentication.html?context=wx)
+API_KEY = "<your API key>"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE:  manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"messages":[{"content":"","role":""}]}
+
+response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/44311d97-40f4-4593-b82f-2c86db0eb9e6/ai_service_stream?version=2021-05-01', json=payload_scoring,
+ headers={'Authorization': 'Bearer ' + mltoken})
+
+print("Scoring response")
+try:
+    print(response_scoring.json())
+except ValueError:
+    print(response_scoring.text)
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
